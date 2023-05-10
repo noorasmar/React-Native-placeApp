008f3e17bf3e09f76a5fb9eae405cc85e7d21a6b
Noor Asmar <noor_jb@outlook.com>
2023-05-10 11:39:47 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>dada</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
